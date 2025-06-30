9eaca60e5ed27ac38e12b5709f76659e439772ae
danielmr28 <00085920@uca.edu.sv>
2025-06-29 21:41:15 -0600

    Cors Solved

--- src/main/java/com/dog/configuration/SecurityConfiguration.java
+++ src/main/java/com/dog/configuration/SecurityConfiguration.java
@@ -60,7 +60,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://unistayf.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
